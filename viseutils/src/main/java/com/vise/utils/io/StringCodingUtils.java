ebffa4e89a8a236ec3b3651df77489731eb6fbf4
xyy <553252628@qq.com>
2016-12-11 18:01:41 +0800

    first commit

---
+++ viseutils/src/main/java/com/vise/utils/io/StringCodingUtils.java
@@ -0,0 +1,23 @@
+package com.vise.utils.io;
+
+import android.os.Build;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
+public class StringCodingUtils {
+
+    public static byte[] getBytes(String src, Charset charSet) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.GINGERBREAD) {
+            try {
+                return src.getBytes(charSet.name());
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        } else {
+            return src.getBytes(charSet);
+        }
+    }
+
+}
