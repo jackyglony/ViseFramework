0e8fc35ea1576cf9a2f7f383d82ff84cc369bb46
xyy <553252628@qq.com>
2016-12-12 11:46:46 +0800

    first commit

--- app/src/main/java/com/vise/app/MainActivity.java
+++ app/src/main/java/com/vise/app/MainActivity.java
@@ -1,16 +1,7 @@
 package com.vise.app;
 
-import android.content.Intent;
-import android.preference.Preference;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.vise.log.ViseLog;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,35 +13,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
-        printMap();
-        printList();
-        printJson();
-        printXml();
+
     }
 
-    private void printXml() {
-        String xml = "<dawi><xyy><td></td></xyy><tt></tt></dawi>";
-        ViseLog.xml(xml);
-    }
-
-    private void printJson() {
-        String json = "{'first':[{'t':'xx'},{'h':'ff'}],'second':{'third':'third','four':'four'}}";
-        ViseLog.json(json);
-    }
-
-    private void printList() {
-        List<String> list = new ArrayList<>();
-        for(int i = 0; i < 5; i++){
-            list.add("dawi"+i);
-        }
-        ViseLog.d(list);
-    }
-
-    private void printMap() {
-        Map<String, String> map = new HashMap<>();
-        for(int i = 0; i < 5; i++){
-            map.put("dawi"+i, "xyy"+i);
-        }
-        ViseLog.d(map);
-    }
 }
